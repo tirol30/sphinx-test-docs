cdbb8ed77a0764f46d05078eed82c1135bd64219
tirol30 <awaya529@yahoo.co.jp>
2024-04-20 23:17:38 +0900

    initial commit

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os, sys
+
+sys.path.append("../src")
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "my-moddd"
+copyright = "2024, tirol30"
+author = "tirol30"
+release = "1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+extensions = [
+    "sphinx.ext.autodoc",  # ソースコード読み込み用
+    "sphinx.ext.napoleon",  # docstring パース用
+    "sphinx_rtd_theme",  # Read the Docs テーマ (今回は不要*1)
+    # "sphinx_multiversion",  # マルチバージョン用
+]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
---
+++ src/my_mod_1/mod_1_1.py
@@ -0,0 +1,18 @@
+def test_func(n: int):
+    """
+    aaa
+    """
+    print("ok")
+    return n * 2
+
+
+class TestClass:
+    """
+    bbb
+    """
+
+    def __init__(self) -> None:
+        """
+        ccc
+        """
+        pass
